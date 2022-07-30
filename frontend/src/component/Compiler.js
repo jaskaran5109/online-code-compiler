84aa266cbf54235c5ebd5852a4b8a9eedf25fea1
jaskaran5109 <67426155+jaskaran5109@users.noreply.github.com>
2022-07-30 22:12:59 +0530

    make it better

--- frontend/src/component/Compiler.js
+++ frontend/src/component/Compiler.js
@@ -59,7 +59,7 @@ const Compiler = () => {
         const { success, job, error } = dataRes;
         if (success) {
           const { status: jobStatus, output: jobOutput } = job;
-
+          console.log(job)
           setstatus(jobStatus);
           setjobDetails(job);
           if (jobStatus === "pending") {
